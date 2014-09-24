3d643a89dcbe4b37775b6ce091fb34a5332d67cb
vikram1134 <vikram.s.1134@gmil.com>
2014-09-24 11:35:08 -0700

    external storage change

--- src/com/red/hot/mobile/peppers/rockpapersissors/DBHelper.java
+++ src/com/red/hot/mobile/peppers/rockpapersissors/DBHelper.java
@@ -3,10 +3,11 @@ package com.red.hot.mobile.peppers.rockpapersissors;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Environment;
 
 public class DBHelper extends SQLiteOpenHelper {
 
-	private static final String DATABASE_NAME = "usersdb";
+	private static final String DATABASE_NAME = Environment.getExternalStorageDirectory()+"/redhotchill/usersdb";
 	private static final int DATABASE_VERSION = 1;
 	private static final String DATABASE_CREATE = "CREATE TABLE USER (username text primary key not null,age text not null, gender text not null, wins int not null, losses int not null);";
 	public DBHelper(Context context) {
